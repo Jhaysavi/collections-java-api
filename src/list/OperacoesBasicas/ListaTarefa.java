53c7d8b5e3f572f362a79efd7a8afac03d034fa7
Jhay Savi <jansavinon@outlook.com>
2024-07-25 19:07:37 -0300

    List catálogo livros challenge

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -1,4 +1,4 @@
-package main.java.list;
+package list.OperacoesBasicas;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/list/OperacoesBasicas/Tarefa.java
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -1,4 +1,4 @@
-package main.java.list;
+package list.OperacoesBasicas;
 
 public class Tarefa {
     //Atributo
