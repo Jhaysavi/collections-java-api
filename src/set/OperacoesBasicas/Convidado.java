a537ed32cd91a66e5c905ee4acbefafa8d7d5d4e
Jhay Savi <jansavinon@outlook.com>
2024-07-26 02:00:19 -0300

    Set convidados challenge

---
+++ src/set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,46 @@
+package set.OperacoesBasicas;
+
+import java.util.Objects;
+
+public class Convidado {
+    //Atributos
+    private String nome;
+    private int codigoConvite;
+
+    //Construtor
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    //Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigoConvite());
+    }
+
+    //To string
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+}
