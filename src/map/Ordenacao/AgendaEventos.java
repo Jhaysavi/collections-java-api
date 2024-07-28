488bf19ee333beeba163345d0046fb9a70cf2d58
Jhay Savi <jansavinon@outlook.com>
2024-07-28 15:39:31 -0300

    Map agenda challenge

---
+++ src/map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,57 @@
+package map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+    //Atributo
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEventos() {
+        this.eventoMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        Evento evento = new Evento(nome, atracao);
+        eventoMap.put(data, evento);
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventosTreeMap);
+
+    }
+
+    public void obterProximoEvento() {
+       /* Set<LocalDate> dataSet = eventoMap.keySet();
+        Collection<Evento> values = eventoMap.values(); */
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.JULY, 29), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 7 , 30), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 7 , 26), "Evento 3", "Atração 3");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+}
